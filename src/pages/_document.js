a7de1d2f351c01de157685ffb5e2de3fff5a11c7
Aminudin Fadila <57476855+fadilaaaa@users.noreply.github.com>
2023-10-18 05:49:49 +0700

    update tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5C2VZCQ"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <Main />
         <NextScript />
       </body>
