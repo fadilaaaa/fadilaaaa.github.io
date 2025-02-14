45dd2187d02df1b61332061dcbd12c291b0c3b89
fadilaaaa <57476855+fadilaaaa@users.noreply.github.com>
2025-02-14 09:43:49 +0700

    update cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
                 </p>
 
                 <div className="flex items-center self-start mt-2 lg:self-center">
-                  <Link href="/resume/CV.pdf" 
+                  <Link href="/resume/CV_en.pdf" 
                   target="_blank"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
                   md:p-2 md:px-4 md:text-base
